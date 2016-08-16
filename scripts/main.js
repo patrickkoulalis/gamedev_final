cd3be81a9ffb7eab4f11634cd504e1e8ead452c2
Patrick Koulalis <Skineboyproblems@gmail.com>
2016-08-16 17:36:08 -0400

    update main javascript file

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,6 @@ $(document).ready(function() {
   $('#news').css('padding-bottom', nmargin);
 });
 
-$(".mobile-nav-toggle").on("click", function(){
-	$("body").toggleClass("mobile-nav");
-});
-
-$('.mobile-nav-toggle').click(function(){
-	$(this).toggleClass('mobile-nav-active');
-});
-
-$("[class^=color-]").hover(function(){
-	$()
-});
-
 $(".news-post-title").click(function(){
 	$("#news").load("news-post.html #news > *");
 });
